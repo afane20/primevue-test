774f780a39b21c8dc1f842879bfe2153ede12654
Ernesto Afane <salvador.afane@gmail.com>
2022-07-20 07:01:24 -0600

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>This is a test</h1>
+  <Button label="Testing"></Button><br><br>
+
+  <!-- <div>
+    <Button type="button" icon="pi pi-plus" label="Expand All" @click="expandAll" />
+    <Button type="button" icon="pi pi-minus" label="Collapse All" @click="collapseAll" />
+  </div> -->
+
+  <Tree :value="nodes"></Tree><br><br>
+  <Tree :value="nodes2"></Tree>
+  <pre style="text-align: left">{{ JSON.stringify(rawData, null, 2) }}</pre>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Button from 'primevue/button';
+import Tree from 'primevue/tree';
+import NodeService from './service/NodeService';
+import NodeService2 from './service/NodeService2';
+import RawTree from './data/demo-data.json';
+
 
 export default {
+ data() {
+        return {
+            nodes: null,
+            nodes2: null,
+            rawData: RawTree
+        }
+    },
+    nodeService: null,
+    nodeService2: null,
+    created() {
+        this.nodeService = new NodeService();
+        this.nodeService2 = new NodeService2();
+    },
+    mounted() {
+        this.nodeService.getTreeNodes().then(data => this.nodes = data);
+        this.nodeService2.getTreeNodes().then(data => this.nodes2 = data);
+    },
+  
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Button, Tree
+  }, 
+  
 }
 </script>
 
